34111d638e492d6e6d02a6cdbeb19134ce38e73c
francisrigor96 <42127741+francisrigor96@users.noreply.github.com>
2022-04-26 22:42:36 -0400

    Add files via upload

---
+++ courseReport.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#include <set>
+#include <cstdio>
+
+using namespace std;
+
+void sayHello();
+void printInput(vector<string> v);
+void getPassingRate(set<string> set, vector<string> vGrade, vector<string> vInstructor);
+void getRateOfW(set<string> set, vector<string> vGrade, vector<string> vInstructor);
+
